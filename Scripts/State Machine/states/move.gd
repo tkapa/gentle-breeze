57f89e73c05d687d7c987b8be9f0b8c54e2709b0
Tylah Kapa <tylahkapa@gmail.com>
2025-07-20 22:20:35 +1000

    Coyote Time

--- Scripts/State Machine/states/move.gd
+++ Scripts/State Machine/states/move.gd
@@ -1,11 +1,21 @@
 extends State
 
+const COYOTE_TIME := 0.2
+
 @export var idle_state: State
 @export var jump_state: State
 @export var fall_state: State
 @export var dash_state: State
 
-const COYOTE_TIME := 0.1
+var _coyote_timer: Timer
+var _coyote_time_started := false
+
+func _ready():
+	_coyote_timer = Timer.new()
+	_coyote_timer.autostart = false;
+	_coyote_timer.one_shot = true;
+	_coyote_timer.wait_time = COYOTE_TIME
+	add_child(_coyote_timer)
 
 func process_input(event: InputEvent) -> State:
 	if get_dash(): 
@@ -13,9 +23,9 @@ func process_input(event: InputEvent) -> State:
 	return null;
 
 func process_physics(delta: float) -> State:
-	if get_jump() and parent.is_on_floor():
+	if get_jump() and _can_jump():
 		return jump_state
-	
+		
 	parent.velocity.y += gravity * delta
 	
 	var movement = get_movement_input().x * move_speed
@@ -28,5 +38,21 @@ func process_physics(delta: float) -> State:
 	parent.move_and_slide()
 	
 	if !parent.is_on_floor():
-		return fall_state
+		if !_coyote_time_started and _coyote_timer.is_stopped():
+			_start_coyote_time()
+			return null
+			
+		if _coyote_time_started and _coyote_timer.is_stopped():
+			return fall_state
+	else:
+		if _coyote_time_started:
+			_coyote_time_started = false
 	return null
+
+func _can_jump() -> bool:
+	var canJump = (parent.is_on_floor() or !_coyote_timer.is_stopped())
+	return canJump
+
+func _start_coyote_time():
+	_coyote_timer.start()
+	_coyote_time_started = true
